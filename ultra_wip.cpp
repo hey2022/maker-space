75d4f3288790343a3b72dee4b7cdee0bde8c145d
Jamey <48553457+hey2022@users.noreply.github.com>
2022-05-16 11:36:16 +0800

    Update ultra_wip.cpp

--- ultra_wip.cpp
+++ ultra_wip.cpp
@@ -1,22 +1,50 @@
-int get_distance();
+int get_distance(int echo, int trig);
 
 void setup() {
   Serial.begin(9600);
-  pinMode(9, OUTPUT);
+  pinMode(6, INPUT);
+  pinMode(7, OUTPUT);
   pinMode(8, INPUT);
+  pinMode(9, OUTPUT);
 }
 
 void loop() {
-  int distance = get_distance();
-  Serial.println(distance);
+  unsigned long long miliseconds = millis();
+  int left_distance = get_distance(6, 7);
+  int right_distance = get_distance(8, 9);
+//  Serial.print("Left: ");
+//  Serial.print(left_distance);
+//  Serial.print(" Right: ");
+//  Serial.print(right_distance);
+//  Serial.print("\n");
+  if (left_distance < max_distance) {
+    t1 = millis();
+  } 
+  else if (d2 < max_distance) {
+    t2 = millis();
+  }
+  
+  if (t1 > 0 && t2 > 0) {
+    if (t1 < t2) {
+      Serial.println("Left to right");
+    } 
+    else if (t2 < t1) {
+      Serial.println("Right to left");
+    }
+    else {
+      Serial.println("");
+    }
+    t1 = 0;
+    t2 = 0;
+  }
 }
 
-int get_distance() {
-  digitalWrite(9, LOW);
+int get_distance(int echo, int trig) {
+  digitalWrite(trig, LOW);
   delayMicroseconds(1);
-  digitalWrite(9, HIGH);
+  digitalWrite(trig, HIGH);
   delayMicroseconds(10);
-  digitalWrite(9, LOW);
-  int T = float (pulseIn(9, HIGH, 18000)) / 58;
+  digitalWrite(trig, LOW);
+  int T = float (pulseIn(echo, HIGH, 18000)) / 58;
   return T;
 }
